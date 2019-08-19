ccc9b58213657d6e6c4b8720df3838e7e705e46f
Abu-Maestro <engryusufmayowa@gmail.com>
2019-08-19 10:59:13 +0100

    Second Commit: Displaying Manchester United Perfected Commiting with Visual Studio

---
+++ Controllers/ClubsController.cs
@@ -0,0 +1,19 @@
+﻿using FootBall.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootBall.Controllers
+{
+    public class ClubsController : Controller
+    {
+        // GET: Clubs
+        public ActionResult Index()
+        {
+            var clubs = new Clubs() { Name = "Manchester United" };
+            return View(clubs);
+        }
+    }
+}
